a6bbebb8949e8341a5e2404c1adb8346e64b155a
Vera <v.khotsko@gmail.com>
2023-05-27 21:00:44 +0300

    Добавила проверку числа от преподавателя

--- task2.py
+++ task2.py
@@ -3,10 +3,15 @@
 100 -> 1 (1 + 0 + 0)"""
 
 number = int(input("Введите трёхзначное число: "))
+
 digit1 = number % 10
 digit2 = number // 100
-digit3 = number // 10 % 10
+digit3 = ( number // 10) % 10
 if number < 100 or number > 999:
     print("Введённое число не трёхзначное. Повторите ввод.")
 else:
     print(f"Сумма всех цифр в числе {number} равна {digit1 + digit2 + digit3}")     
+"""
+while number < 100 or number > 999:
+    number = int(input("Это не трёхзначное число. Повторите ввод: "))
+print(number)"""
